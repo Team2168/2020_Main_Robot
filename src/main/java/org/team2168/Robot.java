ca71b86d854fbccac1e9971652942d43ed3b7c13
James Corcoran <jcorcoran@thayermahan.com>
2020-01-10 19:05:37 -0500

    Initial tests. Over and undershoots a bit. Probably worth testing our how a trapezoidal motion profile works out.

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -7,6 +7,11 @@
 
 package org.team2168;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -24,6 +29,24 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  /** Hardware */
+	TalonSRX _talon = new TalonSRX(11);
+	Joystick _joy = new Joystick(0);
+	
+  /** Used to create string thoughout loop */
+	StringBuilder _sb = new StringBuilder();
+	int _loops = 0;
+	
+  /** Track button state for single press event */
+	boolean _lastButton1 = false;
+
+	/** Save the target position to servo to */
+	double targetPositionRotations;
+
+
+  final double TICKS_PER_REV = 256.0 * 4.0;
+  final double NUM_REVOLUTIONS = 1.0; 
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -33,6 +56,56 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    /* Factory Default all hardware to prevent unexpected behaviour */
+		_talon.configFactoryDefault();
+		
+		/* Config the sensor used for Primary PID and sensor direction */
+        _talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 
+                                            Constants.kPIDLoopIdx,
+				                                    Constants.kTimeoutMs);
+
+		/* Ensure sensor is positive when output is positive */
+		_talon.setSensorPhase(Constants.kSensorPhase);
+
+		/**
+		 * Set based on what direction you want forward/positive to be.
+		 * This does not affect sensor phase. 
+		 */ 
+		_talon.setInverted(Constants.kMotorInvert);
+
+		/* Config the peak and nominal outputs, 12V means full */
+		_talon.configNominalOutputForward(0, Constants.kTimeoutMs);
+		_talon.configNominalOutputReverse(0, Constants.kTimeoutMs);
+		_talon.configPeakOutputForward(Constants.kGains.kPeakOutput, Constants.kTimeoutMs);
+		_talon.configPeakOutputReverse(-Constants.kGains.kPeakOutput, Constants.kTimeoutMs);
+
+		/**
+		 * Config the allowable closed-loop error, Closed-Loop output will be
+		 * neutral within this range. See Table in Section 17.2.1 for native
+		 * units per rotation.
+		 */
+		_talon.configAllowableClosedloopError(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
+
+		/* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
+		_talon.config_kF(Constants.kPIDLoopIdx, Constants.kGains.kF, Constants.kTimeoutMs);
+		_talon.config_kP(Constants.kPIDLoopIdx, Constants.kGains.kP, Constants.kTimeoutMs);
+		_talon.config_kI(Constants.kPIDLoopIdx, Constants.kGains.kI, Constants.kTimeoutMs);
+		_talon.config_kD(Constants.kPIDLoopIdx, Constants.kGains.kD, Constants.kTimeoutMs);
+
+		/**
+		 * Grab the 360 degree position of the MagEncoder's absolute
+		 * position, and intitally set the relative sensor to match.
+		 */
+		int absolutePosition = _talon.getSensorCollection().getPulseWidthPosition();
+
+		/* Mask out overflows, keep bottom 12 bits */
+		absolutePosition &= 0xFFF;
+		if (Constants.kSensorPhase) { absolutePosition *= -1; }
+		if (Constants.kMotorInvert) { absolutePosition *= -1; }
+		
+		/* Set the quadrature (relative) sensor to match absolute */
+		_talon.setSelectedSensorPosition(absolutePosition, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
   }
 
   /**
@@ -86,6 +159,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    commonLoop();
   }
 
   /**
@@ -94,4 +168,76 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
   }
+
+  void commonLoop() {
+		/* Gamepad processing */
+		double leftYstick = _joy.getY();
+		boolean button1 = _joy.getRawButton(1);	// X-Button
+		boolean button2 = _joy.getRawButton(2);	// A-Button
+
+		/* Get Talon/Victor's current output percentage */
+		double motorOutput = _talon.getMotorOutputPercent();
+
+		/* Deadband gamepad */
+		if (Math.abs(leftYstick) < 0.10) {
+			/* Within 10% of zero */
+			leftYstick = 0;
+		}
+
+		/* Prepare line to print */
+		_sb.append("\tout:");
+		/* Cast to int to remove decimal places */
+		_sb.append((int) (motorOutput * 100));
+		_sb.append("%");	// Percent
+
+		_sb.append("\tpos:");
+		_sb.append(_talon.getSelectedSensorPosition(0));
+		_sb.append("u"); 	// Native units
+
+		/**
+		 * When button 1 is pressed, perform Position Closed Loop to selected position,
+		 * indicated by Joystick position x10, [-10, 10] rotations
+		 */
+		if (!_lastButton1 && button1) {
+			/* Position Closed Loop */
+
+			/* number of revs * ticks/rev in either direction */
+			targetPositionRotations = leftYstick * NUM_REVOLUTIONS * TICKS_PER_REV;
+			_talon.set(ControlMode.Position, targetPositionRotations);
+		}
+
+		/* When button 2 is held, just straight drive */
+		if (button2) {
+			/* Percent Output */
+
+			_talon.set(ControlMode.PercentOutput, leftYstick);
+		}
+
+		/* If Talon is in position closed-loop, print some more info */
+		if (_talon.getControlMode() == ControlMode.Position) {
+			/* ppend more signals to print when in speed mode. */
+			_sb.append("\terr:");
+			_sb.append(_talon.getClosedLoopError(0));
+			_sb.append("u");	// Native Units
+
+			_sb.append("\ttrg:");
+			_sb.append(targetPositionRotations);
+			_sb.append("u");	/// Native Units
+		}
+
+		/**
+		 * Print every ten loops, printing too much too fast is generally bad
+		 * for performance.
+		 */
+		if (++_loops >= 10) {
+			_loops = 0;
+			System.out.println(_sb.toString());
+		}
+
+		/* Reset built string for next loop */
+		_sb.setLength(0);
+		
+		/* Save button state for on press detect */
+		_lastButton1 = button1;
+    }
 }
