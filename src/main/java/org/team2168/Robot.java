67d2f1cec9dfccc85cbbcede81ff3da752cbea92
Cierra <cc362x@gmail.com>
2020-01-15 19:40:55 -0500

    Created drive with joystick command

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -25,7 +25,7 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  public static Hopper hopper;
+  private static Hopper hopper;
 
   /**
    * This function is run when the robot is first started up and should be
