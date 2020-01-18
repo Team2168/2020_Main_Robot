288c359ec0a13af74704f352be01c213b3bc6d90
Nathan Kimpan <Nathankimpan@gmail.com>
2020-01-17 20:24:54 -0500

    commited one more time

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -37,7 +37,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    /**  indexer = Indexer.GetInstance();
+    //  indexer = Indexer.GetInstance();
 
   }
 
