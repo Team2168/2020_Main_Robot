6e403aca6fff29443d664e9db11d93d568a0e324
Cooper Halvordson <cjhalvord@gmail.com>
2020-01-18 14:15:34 -0500

    Reverted the FIRST Header.

--- src/main/java/org/team2168/subsystems/Indexer.java
+++ src/main/java/org/team2168/subsystems/Indexer.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. EVERY SINGLE Right Big or Small is Reserved.     */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
