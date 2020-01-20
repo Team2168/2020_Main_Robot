38f36a177fc0aad7ba0dc1e434f1f467e2653d57
James Corcoran <jcorcoran@thayermahan.com>
2020-01-19 19:53:03 -0500

    case statement logic was not executing correctly. Was driving in tank drive when we wanted gunStyle Simplified to just use the only control style we need at the moment.

--- src/main/java/org/team2168/commands/drivetrain/DriveWithJoystick.java
+++ src/main/java/org/team2168/commands/drivetrain/DriveWithJoystick.java
@@ -8,36 +8,25 @@
 package org.team2168.commands.drivetrain;
 
 import org.team2168.OI;
-import org.team2168.Robot;
 import org.team2168.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithJoystick extends Command 
 {
-  int ctrlStyle;
-
-  /**
-   * Controller Styles 0 = Tank Drive (Default) 1 = Gun Style 2 = Arcade Drive 3 =
-   * GTA
-   */
-  //private double speed;
-
-  double rightSpeed = 0;
-  double leftSpeed = 0;
-  private int intCounter = 0;
-  private boolean finished;
+  private Drivetrain dt;
+  private OI oi;
   
   public DriveWithJoystick() 
   {
-    requires(Drivetrain.getInstance());
+    dt = Drivetrain.getInstance();
+    requires(dt);
   }
 
   // Called just before this Command runs the first time
   @Override
-  protected void initialize() 
-  {
- 
+  protected void initialize() {
+    oi = OI.getInstance();
 	}
 
 	/**
@@ -48,117 +37,27 @@ public class DriveWithJoystick extends Command
 	 */
   @Override
   protected void execute() {
-    //double headingCorrection = 0.0;
-    ctrlStyle = Robot.getControlStyleInt();
-
-    switch (ctrlStyle) {
-    /**
-     * Tank Drive
-     */
-    case 0:
-      Drivetrain.getInstance().driveLeft(OI.getInstance().driverJoystick.getLeftStickRaw_Y());
-      Drivetrain.getInstance().driveRight(OI.getInstance().driverJoystick.getRightStickRaw_Y());
-      break;
-
-    /**
-     * Gun Style Controller
-     */
-    // X Values
-    // full in: -0.516
-    // nothing: 0.354 & 0.342
-    // full out: 0.622
-    case 1: {
-      // lastRotateOutput =
-      // Drivetrain.getInstance().rotateDriveStraightController.getControlOutput();
-      // headingCorrection =
-      // (Drivetrain.getInstance().rotateDriveStraightController.getControlOutput());
-
-      Drivetrain.getInstance().tankDrive(
-          (OI.getInstance().getGunStyleYValue()) + OI.getInstance().driverJoystick.getLeftStickRaw_X(),
-          (OI.getInstance().getGunStyleYValue()) - OI.getInstance().driverJoystick.getLeftStickRaw_X());
-    }
-
-      break;
-
-    /**
-     * Arcade Drive
-     */
-    case 2:
-      Drivetrain.getInstance().driveLeft(
-          OI.getInstance().driverJoystick.getLeftStickRaw_Y() + OI.getInstance().driverJoystick.getRightStickRaw_X());
-      Drivetrain.getInstance().driveRight(
-          OI.getInstance().driverJoystick.getLeftStickRaw_Y() - OI.getInstance().driverJoystick.getRightStickRaw_X());
-      break;
-    /**
-     * GTA Drive
-     */
-    case 3:
-      double fwdSpeed = OI.getInstance().driverJoystick.getRightTriggerAxisRaw();
-      double revSpeed = OI.getInstance().driverJoystick.getLeftTriggerAxisRaw();
-      double speed = fwdSpeed - revSpeed;
-      double rotation = OI.getInstance().driverJoystick.getRightStickRaw_X();
-
-      // Adjusts angle while moving
-      if (speed != 0 && rotation != 0) {
-        Drivetrain.getInstance().driveLeft(rotation * speed);
-        Drivetrain.getInstance().driveRight(-rotation * speed);
-      }
-      // Allows Robot to spin in place without needing to press in triggers
-      else if (speed == 0 && rotation != 0) {
-        Drivetrain.getInstance().driveLeft(rotation);
-        Drivetrain.getInstance().driveRight(-rotation);
-      }
-      // Allows Robot to drive straight
-      else if (speed != 0 && rotation == 0) {
-        Drivetrain.getInstance().driveLeft(speed);
-        Drivetrain.getInstance().driveRight(speed);
-      }
-      break;
-
-    /**
-     * New Gun Style Controller
-     */
-    case 4:
-      // lastRotateOutput =
-      // Drivetrain.getInstance().rotateDriveStraightController.getControlOutput();
-      // headingCorrection =
-      // (Drivetrain.getInstance().rotateDriveStraightController.getControlOutput());
-      //
-      break;
-
-    default:
-
-    }
-    /**
-     * Defaults to Tank Drive
-     */
-
-    Drivetrain.getInstance().driveLeft(OI.getInstance().driverJoystick.getLeftStickRaw_Y());
-    Drivetrain.getInstance().driveRight(OI.getInstance().driverJoystick.getRightStickRaw_Y());
-
+    dt.tankDrive((oi.getGunStyleYValue()) + oi.driverJoystick.getLeftStickRaw_X(),
+      (oi.getGunStyleYValue()) - oi.driverJoystick.getLeftStickRaw_X());
   }
 
   // Called repeatedly when this Command is scheduled to run
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() 
-  {
+  protected boolean isFinished() {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() 
-  {
-    Drivetrain.getInstance().tankDrive(0.0, 0.0);
-
+  protected void end() {
+    dt.tankDrive(0.0, 0.0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
-  protected void interrupted() 
-  {
+  protected void interrupted() {
     
   }
 }
