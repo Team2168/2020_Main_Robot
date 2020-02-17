711b11f8e1b1246c904f33710f3b2cf43f71585c
James Corcoran <jcorcoran@thayermahan.com>
2020-02-16 22:26:26 -0500

    Create instances of the in Robot on init so teleop woks.

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -133,16 +133,16 @@ public class Robot extends TimedRobot {
 	
     //Init Subsystems
     // climber = Climber.getInstance();
-    // intakeMotor = IntakeMotor.getInstance();
-    // intakePivot = IntakePivot.getInstance();
+    intakeMotor = IntakeMotor.getInstance();
+    intakePivot = IntakePivot.getInstance();
     // balancer = Balancer.getInstance();
-    // indexer = Indexer.getInstance();
-    // hopper = Hopper.getInstance();
+    indexer = Indexer.getInstance();
+    hopper = Hopper.getInstance();
     colorWheel = ColorWheel.getInstance();
-    // colorWheelPivot = ColorWheelPivot.getInstance();
-    // shooter = Shooter.getInstance();
-    // hoodAdjust = HoodAdjust.getInstance();
-    // drivetrain = Drivetrain.getInstance();
+    colorWheelPivot = ColorWheelPivot.getInstance();
+    shooter = Shooter.getInstance();
+    hoodAdjust = HoodAdjust.getInstance();
+    drivetrain = Drivetrain.getInstance();
     oi = OI.getInstance();
     
     // pdp = new PowerDistribution(RobotMap.PDPThreadPeriod);
