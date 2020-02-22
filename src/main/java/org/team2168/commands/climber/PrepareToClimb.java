b5c2d1c4258c0148332240eaeb3a0db088047b92
Alyssa Solomon <amsolomon.ams@gmail.com>
2020-02-22 14:56:11 -0500

    climb testing

--- src/main/java/org/team2168/commands/climber/PrepareToClimb.java
+++ src/main/java/org/team2168/commands/climber/PrepareToClimb.java
@@ -31,7 +31,7 @@ public class PrepareToClimb extends CommandGroup {
     // a CommandGroup containing them would require both the chassis and the
     // arm.
     addSequential(new DisengageRatchet());
-    addSequential(new DriveClimberXPosition(49.0, 0.1));
+    addSequential(new DriveClimberXPosition(48.0, 0.1));
     addSequential(new EngageRatchet());
   }
 }
