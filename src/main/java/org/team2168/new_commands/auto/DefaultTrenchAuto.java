7352599f867959d80a35c46cda18eca580610839
Derek Osborne <legodmo10@gmail.com>
2020-02-27 20:45:10 -0500

    Created new_commands and made auto. Errors due to other required classes not created yet.

---
+++ src/main/java/org/team2168/new_commands/auto/DefaultTrenchAuto.java
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.team2168.new_commands;
+
+import org.team2168.commands.auto.FireBallsAutoNoLineBreak;
+import org.team2168.commands.auto.Sleep;
+import org.team2168.commands.drivetrain.PIDCommands.DriveXDistance;
+import org.team2168.commands.drivetrain.PIDCommands.TurnXAngle;
+import org.team2168.commands.hood_adjust.ExtendShooterHood;
+import org.team2168.commands.hood_adjust.MoveToBackTrench;
+import org.team2168.commands.hood_adjust.MoveToFrontTrench;
+import org.team2168.commands.hood_adjust.RetractShooterHardstop;
+import org.team2168.commands.hopper.DriveHopperWithConstant;
+import org.team2168.commands.indexer.DriveIndexerWithConstant;
+import org.team2168.commands.intakeMotor.DriveIntakeWithConstant;
+import org.team2168.commands.intakePivot.ExtendIntakePneumatic;
+import org.team2168.commands.intakePivot.RetractIntakePneumatic;
+import org.team2168.commands.shooter.DriveToXSpeed;
+import org.team2168.new_subsystems.Shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DefaultTrenchAuto extends CommandBase {
+  /**
+   * Add your docs here.
+   */
+  public DefaultTrenchAuto() {
+    // Add Commands here:
+    // e.g. addSequential(new Command1());
+    // addSequential(new Command2());
+    // these will run in order.
+
+    // To run multiple commands at the same time,
+    // use addParallel()
+    // e.g. addParallel(new Command1());
+    // addSequential(new Command2());
+    // Command1 and Command2 will run in parallel.
+
+    // A command group will require all of the subsystems that each member
+    // would require.
+    // e.g. if Command1 requires chassis, and Command2 requires arm,
+    // a CommandGroup containing them would require both the chassis and the
+    // arm.
+
+    // addParallel(new MoveToFrontTrench());
+    // addParallel(new DriveIntakeWithConstant(0.95));//TODO set
+    // addSequential(new ExtendIntakePneumatic());
+    // addSequential(new DriveXDistance(-115.0, 0.5), 4.0);
+    // addParallel(new DriveIntakeWithConstant(0.3));
+    // addSequential(new RetractIntakePneumatic());  //want to run the intake constantly during firing... TODO: cleanup
+    // addSequential(new TurnXAngle(13.25, 0.3), 2.0); //12.25), 2.0);
+    // addSequential(new FireBallsAutoNoLineBreak(), 2.0);
+
+    // addSequential(new DriveHopperWithConstant(0.0), 0.1);
+    // addParallel(new DriveIndexerWithConstant(0.0), 0.0);
+    // // //moves hood down and leaves shooter at speed 
+    // addSequential(new RetractShooterHardstop());
+    // addSequential(new Sleep(), 0.1);
+    // addSequential(new ExtendShooterHood());
+    // addSequential(new Sleep(), 0.1);
+
+    // addParallel(new DriveToXSpeed(Shooter.getInstance().BACK_TRENCH_VEL));
+
+    // // //turn straight again
+    // addSequential(new TurnXAngle(-13.25, 0.3), 2.0);
+    // addParallel(new DriveIntakeWithConstant(0.95));//TODO set
+    // addSequential(new ExtendIntakePneumatic());
+    // addSequential(new DriveXDistance(-147.0, 0.5, 8.0*12.0), 4.0);
+
+
+    // addParallel(new MoveToBackTrench());
+    // addParallel(new DriveIntakeWithConstant(0.3));
+    // addSequential(new RetractIntakePneumatic()); 
+    // addSequential(new TurnXAngle(9.0, 0.3), 2.0);
+    // addSequential(new FireBallsAutoNoLineBreak(), 4.0);
+  }
+}
---
+++ src/main/java/org/team2168/new_commands/auto/WaitForLineBreaks.java
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.team2168.new_commands;
+
+import org.team2168.new_subsystems.Climber;
+import org.team2168.new_subsystems.Indexer;
+import org.team2168.new_subsystems.IntakeMotor;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class WaitForLineBreaks extends CommandBase {
+  private double _indexerSpeed;
+  private double _hopperSpeed;
+  private double _intakeSpeed;
+  private int _numBalls;
+  private boolean exitLineBreakLast;
+  private int ballCounter;
+  /**
+   * if line break triggers too many times from balls jittering, 
+   * add counter that iterates loops that the ball visible to the line break
+   * above a certain threshold should be a ball actually going through 
+   * and not just jitters
+   */
+  private Indexer indexer;
+  private Climber climber;
+  private IntakeMotor intake;
+  public WaitForLineBreaks(double indexerSpeed, double hopperSpeed, double intakeSpeed, int numBalls) {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    indexer = Indexer.getInstance();
+    climber = Climber.getInstance();
+    intake = IntakeMotor.getInstance();
+    requires(indexer);
+    requires(climber);
+    requires(intake);
+
+    this._indexerSpeed = indexerSpeed;
+    this._hopperSpeed = hopperSpeed;
+    this._intakeSpeed = intakeSpeed;
+    this._numBalls = numBalls;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    exitLineBreakLast = indexer.isBallExiting();
+    ballCounter = 0;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if (exitLineBreakLast && !indexer.isBallExiting())
+    {
+      ballCounter++;
+    }
+
+    exitLineBreakLast = indexer.isBallExiting();
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return ballCounter < _numBalls;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
