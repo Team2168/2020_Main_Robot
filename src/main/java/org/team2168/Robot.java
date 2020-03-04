e1f9aec3dec33eb6be870c023d157d25a9f64450
kalebsoriginalname <kalebsmigal@gmail.com>
2020-03-03 20:27:31 -0500

    hall effect integration into robot 4 teleop

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -214,6 +214,7 @@ public class Robot extends TimedRobot {
 	public void teleopInit() {
     autoMode = false;
     drivetrain.setDefaultBrakeMode();
+    climber.zeroEncoderWhenLiftIsDown();
 
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to 
