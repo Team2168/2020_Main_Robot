0397e2f0c69c1182adc80b27deee72d68da1d059
Caeden Foxhoven <caedentfoxhoven@gmail.com>
2020-03-05 20:16:38 -0500

    Added ColorSensor subsytem

---
+++ src/main/java/org/team2168/subsystems/ColorSensor.java
@@ -0,0 +1,109 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.team2168.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.I2C;
+
+
+/**
+ * Add your docs here.
+ */
+public class ColorSensor extends Subsystem {
+
+  private static I2C _arduino;
+  private byte[] _receivedData;
+  private double g_norm = 0;
+  private double r_norm = 0;
+  private double b_norm = 0;
+  private int r = 0;
+  private int g = 0;
+  private int b = 0;
+  private static ColorSensor instance = null;
+
+
+
+  private ColorSensor()
+  {
+    _arduino = new I2C(I2C.Port.kOnboard,8);
+    _receivedData  = new byte[3];
+  }
+  public String getColorData()
+  {
+    _arduino.readOnly(_receivedData, _receivedData.length);
+    r = _receivedData[0] & 0xff;
+    g = _receivedData[1] & 0xff;
+    b = _receivedData[2] & 0xff;
+
+    float MAX;  
+    int NORM_SCALE= 255;
+    
+
+
+    MAX = r;
+  
+    if (g > MAX){
+      MAX = g;
+    }
+    
+    if (b> MAX){
+      MAX = b;
+    }
+    
+    if (MAX > 0){
+      r_norm = NORM_SCALE*r/MAX;
+      g_norm = NORM_SCALE*g / MAX;
+      b_norm = NORM_SCALE*b / MAX;    
+    }
+    
+    else{
+      r_norm = 0;
+      g_norm = 0;
+      b_norm = 0;  
+    }
+    return (calculateColorData(r_norm, g_norm, b_norm));
+  }
+    
+  public String calculateColorData(double r, double g, double b){
+    if (r_norm ==255 && g_norm <200 && b_norm <200) {
+      return("Red");
+    }
+  
+    else if (g_norm ==255 && r_norm <200 &&(b_norm <150)) {
+      return("Green");
+    }
+  
+    else if ((r_norm ==255 && (g_norm>200) && b_norm <150) || (g_norm ==255 && (r_norm>200) && b_norm <150)){
+      //Serial.print ("Yellow ");  
+      return("Yellow");
+      // RGB_color(200,150,0); // LED
+    }
+  
+    else if (b_norm ==255 && r_norm <200  && g_norm <200) {
+      //Serial.print ("Blue "); 
+      return("Blue");
+      // RGB_color(0,0,255); // LED
+    }
+    else 
+      return("?");  
+  }
+
+  public static ColorSensor getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new ColorSensor();
+    }
+    return instance;
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
