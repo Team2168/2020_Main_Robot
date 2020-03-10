8bf7a80645b51aab7ac6f046d7e86a38a31aa216
Caeden Foxhoven <caedentfoxhoven@gmail.com>
2020-03-10 17:45:06 -0400

    Added java docs to methods

--- src/main/java/org/team2168/subsystems/ColorSensor.java
+++ src/main/java/org/team2168/subsystems/ColorSensor.java
@@ -11,9 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.I2C;
 
 
-/**
- * Add your docs here.
- */
+
 public class ColorSensor extends Subsystem {
 
   private static I2C _arduino;
@@ -33,6 +31,11 @@ public class ColorSensor extends Subsystem {
     _arduino = new I2C(I2C.Port.kOnboard,8);
     _receivedData  = new byte[3];
   }
+
+/**
+ * Takes in the data from the color wheel sensor, normalizes to a degree of 255,
+ * then runs and returns calculateColorData() with the data it just received
+ */
   public String getColorData()
   {
     _arduino.readOnly(_receivedData, _receivedData.length);
@@ -68,7 +71,12 @@ public class ColorSensor extends Subsystem {
     }
     return (calculateColorData(r_norm, g_norm, b_norm));
   }
-    
+  
+/**
+ * @return a string either saying Red, Green, Blue, or Yellow based on how much of each color was provided.
+ * If the values are outside that of these four colors, @return a ? instead as an error
+ */
+
   public String calculateColorData(double r, double g, double b){
     if (r_norm ==255 && g_norm <200 && b_norm <200) {
       return("Red");
@@ -92,6 +100,10 @@ public class ColorSensor extends Subsystem {
     else 
       return("?");  
   }
+
+  /**
+   * @return an instance of the ColorSensor Subsystem
+   */
 
   public static ColorSensor getInstance()
   {
